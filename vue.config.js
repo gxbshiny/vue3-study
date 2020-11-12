22d2e9d270aa75e95a4831efbbc37c78cb6e43e6
gxb <gxbshiny@163.com>
2020-11-12 18:15:31 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+    lintOnSave: process.env.NODE_ENV !== 'production',
+    productionSourceMap = false
+
+}
